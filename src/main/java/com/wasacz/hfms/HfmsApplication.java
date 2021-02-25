a992c218977f06da4b7a2b9737ecd8d7f3208c10
MrWonsik <mrt.wonsik@gmail.com>
2021-02-25 20:07:16 +0100

    spring boot initialization

---
+++ src/main/java/com/wasacz/hfms/HfmsApplication.java
@@ -0,0 +1,13 @@
+package com.wasacz.hfms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HfmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wasacz/hfms/HfmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasacz.hfms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
