a2f3d390b6cd9aa4b87b24e9bbd649297a300cd3
MrWonsik <mrt.wonsik@gmail.com>
2021-05-28 20:37:26 +0200

    Z011 - enhancemenets randomizer

--- src/main/java/com/wasacz/hfms/utils/ExampleUserFactory.java
+++ src/main/java/com/wasacz/hfms/utils/ExampleUserFactory.java
@@ -2,6 +2,7 @@ package com.wasacz.hfms.utils;
 
 import com.wasacz.hfms.finance.category.CategoryServiceFactory;
 import com.wasacz.hfms.finance.category.CategoryType;
+import com.wasacz.hfms.finance.category.controller.AbstractCategoryResponse;
 import com.wasacz.hfms.finance.category.controller.CategoriesResponse;
 import com.wasacz.hfms.finance.transaction.TransactionServiceFactory;
 import com.wasacz.hfms.finance.transaction.TransactionType;
@@ -11,12 +12,14 @@ import com.wasacz.hfms.persistence.User;
 import com.wasacz.hfms.persistence.UserRepository;
 import com.wasacz.hfms.user.management.controller.CreateUserRequest;
 import com.wasacz.hfms.user.management.service.UserManagementService;
+import com.wasacz.hfms.utils.randomizer.Randomizer;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -24,16 +27,25 @@ import java.util.concurrent.ThreadLocalRandom;
 @Service
 public class ExampleUserFactory {
 
+    private final Random random = new Random();
+    private final HashMap<String, Randomizer.Properties> incomeCategories;
+    private final HashMap<String, Randomizer.Properties> expenseCategories;
+
+
     private final UserManagementService userManagementService;
     private final TransactionServiceFactory transactionServiceFactory;
     private final UserRepository userRepository;
     private final CategoryServiceFactory categoryServiceFactory;
+    private final Randomizer randomizer;
 
-    public ExampleUserFactory(UserManagementService userManagementService, TransactionServiceFactory transactionServiceFactory, UserRepository userRepository, CategoryServiceFactory categoryServiceFactory) {
+    public ExampleUserFactory(UserManagementService userManagementService, TransactionServiceFactory transactionServiceFactory, UserRepository userRepository, CategoryServiceFactory categoryServiceFactory, Randomizer randomizer) {
         this.userManagementService = userManagementService;
         this.transactionServiceFactory = transactionServiceFactory;
         this.userRepository = userRepository;
         this.categoryServiceFactory = categoryServiceFactory;
+        this.randomizer = randomizer;
+        incomeCategories = randomizer.getIncomeCategories();
+        expenseCategories = randomizer.getExpenseCategories();
     }
 
 
@@ -63,51 +75,118 @@ public class ExampleUserFactory {
     }
 
     private void createExampleTransactions(User user) {
-        createExampleExpenseTransactions(user, 200);
-        createExampleIncomeTransactions(user, 200);
+        createExampleExpenseTransactions(user);
+        createExampleIncomeTransactions(user);
     }
 
-    private void createExampleIncomeTransactions(User user, int quantity) {
+    private void createExampleIncomeTransactions(User user) {
         CategoriesResponse allIncomeCategories = categoryServiceFactory.getService(CategoryType.INCOME).getAllCategories(user);
-        for (int i=0; i<quantity;i++) {
-            IncomeObj transaction = createRandomIncome(allIncomeCategories);
-            transactionServiceFactory.getService(TransactionType.INCOME).add(transaction, user, null);
+        createSalaryIncomes(user, allIncomeCategories);
+        createRandomIncomes(user, allIncomeCategories, "Selling", 4);
+        createRandomIncomes(user, allIncomeCategories, "Gifts", 4);
+    }
+
+    private void createSalaryIncomes(User user, CategoriesResponse allIncomeCategories) {
+        AbstractCategoryResponse categoryAbstract = getCategoryByName(allIncomeCategories, "Salary");
+        if (categoryAbstract == null) return;
+        Randomizer.Properties categoryProperties = incomeCategories.get( categoryAbstract.getCategoryName());
+        Randomizer.Product product = categoryProperties.getProducts().get(0); // It's salary index
+        LocalDate date = LocalDate.now();
+        for(int i=0; i<25; i++) {
+            IncomeObj income = IncomeObj.builder()
+                    .name(product.getName())
+                    .amount(Math.random() * (product.getMaxAmount().doubleValue() - product.getMinAmount().doubleValue()) + product.getMinAmount().doubleValue())
+                    .categoryId(categoryAbstract.getId())
+                    .transactionType("INCOME")
+                    .transactionDate(date)
+                    .build();
+            transactionServiceFactory.getService(TransactionType.INCOME).add(income, user, null);
+            date = date.minusMonths(1L);
         }
     }
 
-    private IncomeObj createRandomIncome(CategoriesResponse allCategories) {
-        Random random = new Random();
-        return IncomeObj.builder()
-                .name("Random income")
-                .amount(2000 * random.nextDouble())
-                .categoryId(allCategories.getCategories().get(random.nextInt(allCategories.getCategories().size())).getId())
-                .transactionType("INCOME")
-                .transactionDate(getRandomDate())
-                .build();
+    private void createRandomIncomes(User user, CategoriesResponse allCategories, String categoryName, int quantity) {
+        AbstractCategoryResponse category = getCategoryByName(allCategories, categoryName);
+        if (category == null) return;
+        Randomizer.Properties categoryProperties = incomeCategories.get( category.getCategoryName());
+        Randomizer.Product product = categoryProperties.getProducts().get(random.nextInt(categoryProperties.getProducts().size()));
+        for(int i=0; i<quantity; i++) {
+            IncomeObj income = IncomeObj.builder()
+                    .name(product.getName())
+                    .amount(Math.random() * (product.getMaxAmount().doubleValue() - product.getMinAmount().doubleValue()) + product.getMinAmount().doubleValue())
+                    .categoryId(category.getId())
+                    .transactionType("INCOME")
+                    .transactionDate(getRandomDate())
+                    .build();
+            transactionServiceFactory.getService(TransactionType.INCOME).add(income, user, null);
+        }
     }
 
-    private void createExampleExpenseTransactions(User user, int quantity) {
+    private void createExampleExpenseTransactions(User user) {
         CategoriesResponse allExpenseCategories = categoryServiceFactory.getService(CategoryType.EXPENSE).getAllCategories(user);
-        for (int i=0; i<quantity;i++) {
-            ExpenseObj transaction = createRandomExpense(allExpenseCategories);
-            transactionServiceFactory.getService(TransactionType.EXPENSE).add(transaction, user, null);
-        }
+        createBillsExpenses(user, allExpenseCategories);
+        createRandomExpense(user, allExpenseCategories, "Grocery Shopping", 200);
+        createRandomExpense(user, allExpenseCategories, "Transport", 50);
+        createRandomExpense(user, allExpenseCategories, "Car", 24);
+        createRandomExpense(user, allExpenseCategories, "Culture and entertainment", 24);
+        createRandomExpense(user, allExpenseCategories, "Health and beauty", 24);
+        createRandomExpense(user, allExpenseCategories, "Personal development", 24);
+        createRandomExpense(user, allExpenseCategories, "Home", 24);
+        createRandomExpense(user, allExpenseCategories, "Clothes and accessories", 48);
+        createRandomExpense(user, allExpenseCategories, "Other", 100);
+
     }
 
-    private ExpenseObj createRandomExpense(CategoriesResponse allCategories) {
-        Random random = new Random();
-        return ExpenseObj.builder()
-                .expenseName("Random expense")
-                .amount(2000 * random.nextDouble())
-                .categoryId(allCategories.getCategories().get(random.nextInt(allCategories.getCategories().size())).getId())
-                .transactionType("EXPENSE")
-                .transactionDate(getRandomDate())
-                .build();
+    private void createBillsExpenses(User user, CategoriesResponse allExpenseCategories) {
+        AbstractCategoryResponse categoryAbstract = getCategoryByName(allExpenseCategories, "Bills");
+        if (categoryAbstract == null) return;
+        Randomizer.Properties categoryProperties = expenseCategories.get( categoryAbstract.getCategoryName());
+        categoryProperties.getProducts().forEach(product -> {
+            LocalDate date = LocalDate.now();
+            for(int i=0; i<25; i++) {
+                ExpenseObj income = ExpenseObj.builder()
+                        .expenseName(product.getName())
+                        .amount(Math.random() * (product.getMaxAmount().doubleValue() - product.getMinAmount().doubleValue()) + product.getMinAmount().doubleValue())
+                        .categoryId(categoryAbstract.getId())
+                        .transactionType("EXPENSE")
+                        .transactionDate(date)
+                        .build();
+                transactionServiceFactory.getService(TransactionType.EXPENSE).add(income, user, null);
+                date = date.minusMonths(1L);
+            }
+        });
+    }
+
+    private void createRandomExpense(User user, CategoriesResponse allCategories, String categoryName, int quantity) {
+        AbstractCategoryResponse category = getCategoryByName(allCategories, categoryName);
+        if (category == null) return;
+        Randomizer.Properties categoryProperties = expenseCategories.get(categoryName);
+        Randomizer.Product product = categoryProperties.getProducts().get(random.nextInt(categoryProperties.getProducts().size()));
+        for(int i=0; i<quantity; i++) {
+            ExpenseObj expense = ExpenseObj.builder()
+                    .expenseName(product.getName())
+                    .amount(Math.random() * (product.getMaxAmount().doubleValue() + product.getMinAmount().doubleValue()) + product.getMinAmount().doubleValue())
+                    .categoryId(category.getId())
+                    .transactionType("EXPENSE")
+                    .transactionDate(getRandomDate())
+                    .build();
+            transactionServiceFactory.getService(TransactionType.EXPENSE).add(expense, user, null);
+        }
+
+    }
+
+    private AbstractCategoryResponse getCategoryByName(CategoriesResponse allCategories, String categoryName) {
+        Optional<? extends AbstractCategoryResponse> foundCategory = allCategories.getCategories().stream().filter(abstractCategoryResponse -> categoryName.contains(abstractCategoryResponse.getCategoryName())).findFirst();
+
+        if (foundCategory.isEmpty()) {
+            return null;
+        }
+        return foundCategory.get();
     }
 
     private LocalDate getRandomDate() {
         Instant now = Instant.now();
-        Instant threeYearAgo = Instant.now().minus(Duration.ofDays(3*365));
+        Instant threeYearAgo = Instant.now().minus(Duration.ofDays(2*365));
         return LocalDate.ofInstant(Instant.ofEpochSecond(ThreadLocalRandom.current().nextLong(threeYearAgo.getEpochSecond(), now.getEpochSecond())), ZoneId.systemDefault());
     }
 }
